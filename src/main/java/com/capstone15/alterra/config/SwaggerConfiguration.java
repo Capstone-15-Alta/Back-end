14915ad61049c69054b6d79bd2072bb63a6980c2
muhammadhendro <muhammadhendro88@gmail.com>
2022-05-30 09:20:56 +0700

    add swagger documentation

---
+++ src/main/java/com/capstone15/alterra/config/SwaggerConfiguration.java
@@ -0,0 +1,54 @@
+package com.capstone15.alterra.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.*;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
+import java.util.List;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfiguration {
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .securityContexts(List.of(securityContext()))
+                .securitySchemes(List.of(apiKey()))
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.capstone15.alterra.controller"))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
+    }
+    private ApiInfo apiInfo() {
+        return new ApiInfo(
+                "Capstone Project FGD",
+                "API for capstone project spring boot .",
+                "V1",
+                "Terms of service",
+                new Contact("Kelompok 15", "https://github.com/Capstone-15-Alta", "mhendro@gmail.com"),
+                "License of API", "API license URL", Collections.emptyList());
+    }
+    private ApiKey apiKey() {
+        return new ApiKey("JWT", "Authorization", "header");
+    }
+
+    private SecurityContext securityContext() {
+        return SecurityContext.builder().securityReferences(defaultAuth()).build();
+    }
+
+    private List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return List.of(new SecurityReference("JWT", authorizationScopes));
+    }
+}
+
