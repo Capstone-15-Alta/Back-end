7fb9a1e4fd78824fb124261b63a1762f691dda5d
muhammadhendro <muhammadhendro88@gmail.com>
2022-06-08 17:35:59 +0700

    feature: add thread follower

--- src/main/java/com/capstone15/alterra/domain/dto/ThreadFollowerDto.java
+++ src/main/java/com/capstone15/alterra/domain/dto/ThreadFollowerDto.java
@@ -1,5 +1,6 @@
 package com.capstone15.alterra.domain.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.AllArgsConstructor;
@@ -8,26 +9,22 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
+import java.time.LocalDateTime;
 
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
-public class ThreadDto implements Serializable {
+public class ThreadFollowerDto implements Serializable {
 
-    private static final long serialVersionUID = -3641627981085086785L;
-    private Long id;
-
-    private String title;
-
-    private String description;
-
-    private String image;
-
-    private Long categoryId;
+    private static final long serialVersionUID = 7903140969561110489L;
 
     private Long userId;
 
+    private Long threadId;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss")
+    private LocalDateTime followTime;
 
 }
