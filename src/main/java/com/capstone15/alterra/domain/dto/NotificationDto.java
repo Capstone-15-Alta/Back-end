36b745b7224e6cf46d816176136d1f0c552f0047
muhammadhendro <muhammadhendro88@gmail.com>
2022-06-27 23:47:02 +0700

    add dto response notification

--- src/main/java/com/capstone15/alterra/domain/dto/NotificationDto.java
+++ src/main/java/com/capstone15/alterra/domain/dto/NotificationDto.java
@@ -1,2 +1,32 @@
-package com.capstone15.alterra.domain.dto;public class NotificationDto {
+package com.capstone15.alterra.domain.dto;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class NotificationDto implements Serializable {
+
+    private static final long serialVersionUID = -841934041447250707L;
+
+    private Long id;
+
+    private Long userId;
+
+    private String title;
+
+    private String message;
+
+    private String createdAt;
+
+    private Boolean isRead;
 }
