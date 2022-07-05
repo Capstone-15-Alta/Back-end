5c9758985155b591e14037821fbb489158374c4e
muhammadhendro <muhammadhendro88@gmail.com>
2022-07-05 20:47:01 +0700

    update unit test user service

--- src/test/java/com/capstone15/alterra/service/UserServiceTest.java
+++ src/test/java/com/capstone15/alterra/service/UserServiceTest.java
@@ -18,10 +18,7 @@ import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -77,25 +74,39 @@ class UserServiceTest {
 
     }
 
-//    @Test
-//    void getAllUserSuccess_Test() {
-//        UserDao userDao = UserDao.builder()
-//                .id(1L)
-//                .build();
-//
-//        Page<UserDao> userDaos = new PageImpl<>(List.of(userDao));
-//        when(userRepository.findAll(PageRequest.of(any(), any()))).thenReturn(userDaos);
-//
-//        ResponseEntity<Object> response = userService.getAllUser(any());
-//
-//        ApiResponse apiResponse = (ApiResponse) response.getBody();
-//
-//        List<UserDto> list = (List<UserDto>) apiResponse.getData();
-//
-//        assertEquals(HttpStatus.OK.value(), response.getStatusCodeValue());
-//        assertEquals(AppConstant.Message.SUCCESS, Objects.requireNonNull(apiResponse).getMessage());
-//        assertEquals(1, list.size());
-//    }
+    @Test
+    void getAllUserSuccess_Test() {
+        UserDao userDao = UserDao.builder()
+                .id(1L)
+                .build();
+
+        Page<UserDao> userDaos = new PageImpl<>(List.of(userDao));
+        when(userRepository.findAll((Pageable) any())).thenReturn(userDaos);
+
+        ResponseEntity<Object> response = userService.getAllUser(any());
+
+        ApiResponse apiResponse = (ApiResponse) response.getBody();
+
+        Page<UserDtoResponse> page = (Page<UserDtoResponse>) apiResponse.getData();
+
+        assertEquals(HttpStatus.OK.value(), response.getStatusCodeValue());
+        assertEquals(AppConstant.Message.SUCCESS, Objects.requireNonNull(apiResponse).getMessage());
+        assertEquals(1, page.getTotalElements());
+    }
+
+    @Test
+    void getAllUserNotFound_Test() {
+        Page<UserDao> userDaos = new PageImpl<>(List.of());
+        when(userRepository.findAll((Pageable) any())).thenReturn(userDaos);
+
+        ResponseEntity<Object> response = userService.getAllUser(any());
+
+        ApiResponse apiResponse = (ApiResponse) response.getBody();
+
+        assertEquals(HttpStatus.BAD_REQUEST.value(), response.getStatusCodeValue());
+        assertEquals(AppConstant.Message.NOT_FOUND, Objects.requireNonNull(apiResponse).getMessage());
+
+    }
 
     @Test
     void getAllUserException_Test() {
@@ -386,4 +397,281 @@ class UserServiceTest {
 
     }
 
+    @Test
+    void updateUserPhotoSuccess_Test() throws IOException {
+        UserDao userDao = UserDao.builder()
+                .id(1L)
+                .build();
+
+        UserDto userDto = UserDto.builder()
+                .id(1L)
+                .username("admin")
+                .password("123")
+                .build();
+
+        UserDtoResponse userDtoResponse = UserDtoResponse.builder()
+                .id(1L)
+                .username("admin")
+                .email("email")
+                .build();
+
+        MockMultipartFile file
+                = new MockMultipartFile(
+                "file",
+                "hello.txt",
+                MediaType.MULTIPART_FORM_DATA_VALUE,
+                "Hello, World!".getBytes()
+        );
+
+        when(userRepository.findById(anyLong())).thenReturn(Optional.of(userDao));
+        when(mapper.map(any(), eq(UserDao.class))).thenReturn(userDao);
+        when(mapper.map(any(), eq(UserDto.class))).thenReturn(userDto);
+        when(mapper.map(any(), eq(UserDtoResponse.class))).thenReturn(userDtoResponse);
+        when(userRepository.save(any())).thenReturn(userDao);
+
+        ResponseEntity<Object> response = userService.updateUserPhoto(file, userDao);
+
+        ApiResponse apiResponse = (ApiResponse) response.getBody();
+
+        UserDtoResponse userDto1 = (UserDtoResponse) Objects.requireNonNull(apiResponse).getData();
+
+        assertEquals(HttpStatus.OK.value(), response.getStatusCodeValue());
+        assertEquals(AppConstant.Message.SUCCESS, Objects.requireNonNull(apiResponse).getMessage());
+        assertEquals(userDto.getUsername(), userDto1.getUsername());
+
+    }
+
+    @Test
+    void updateUserPhotoNotFound_Test() throws IOException {
+        UserDao userDao = UserDao.builder()
+                .id(1L)
+                .build();
+
+        when(userRepository.findById(anyLong())).thenReturn(Optional.empty());
+        ResponseEntity<Object> responseEntity = userService.updateUserPhoto( null, userDao);
+
+        ApiResponse apiResponse = (ApiResponse) responseEntity.getBody();
+
+        assertEquals(HttpStatus.BAD_REQUEST.value(), responseEntity.getStatusCodeValue());
+        assertEquals(AppConstant.Message.NOT_FOUND, Objects.requireNonNull(apiResponse).getMessage());
+
+    }
+
+    @Test
+    void updateUserPhotoDenied_Test() throws IOException {
+        UserDao userDao = UserDao.builder()
+                .id(1L)
+                .username("admin")
+                .roles("USER")
+                .build();
+
+        UserDao userDao2 = UserDao.builder()
+                .id(2L)
+                .username("admin")
+                .roles("USER")
+                .build();
+
+        UserDtoResponse userDtoResponse = UserDtoResponse.builder()
+                .id(2L)
+                .username("admin")
+                .build();
+
+        when(userRepository.findById(anyLong())).thenReturn(Optional.of(userDao2));
+
+        ResponseEntity<Object> responseEntity = userService.updateUserPhoto( null, userDao);
+
+        ApiResponse apiResponse = (ApiResponse) responseEntity.getBody();
+
+        assertEquals(HttpStatus.INTERNAL_SERVER_ERROR.value(), responseEntity.getStatusCodeValue());
+        assertEquals(AppConstant.Message.UNKNOWN_ERROR, Objects.requireNonNull(apiResponse).getMessage());
+
+    }
+
+    @Test
+    void updateUserPhotoException_Test() {
+        UserDao userDao = UserDao.builder()
+                .id(1L)
+                .build();
+        when(userRepository.findById(any())).thenThrow(NullPointerException.class);
+        assertThrows(Exception.class, () -> userService.updateUserPhoto( null, userDao));
+
+    }
+
+    @Test
+    void updateUserCoverSuccess_Test() throws IOException {
+        UserDao userDao = UserDao.builder()
+                .id(1L)
+                .build();
+
+        UserDto userDto = UserDto.builder()
+                .id(1L)
+                .username("admin")
+                .password("123")
+                .build();
+
+        UserDtoResponse userDtoResponse = UserDtoResponse.builder()
+                .id(1L)
+                .username("admin")
+                .email("email")
+                .build();
+
+        MockMultipartFile file
+                = new MockMultipartFile(
+                "file",
+                "hello.txt",
+                MediaType.MULTIPART_FORM_DATA_VALUE,
+                "Hello, World!".getBytes()
+        );
+
+        when(userRepository.findById(anyLong())).thenReturn(Optional.of(userDao));
+        when(mapper.map(any(), eq(UserDao.class))).thenReturn(userDao);
+        when(mapper.map(any(), eq(UserDto.class))).thenReturn(userDto);
+        when(mapper.map(any(), eq(UserDtoResponse.class))).thenReturn(userDtoResponse);
+        when(userRepository.save(any())).thenReturn(userDao);
+
+        ResponseEntity<Object> response = userService.updateUserCover(file, userDao);
+
+        ApiResponse apiResponse = (ApiResponse) response.getBody();
+
+        UserDtoResponse userDto1 = (UserDtoResponse) Objects.requireNonNull(apiResponse).getData();
+
+        assertEquals(HttpStatus.OK.value(), response.getStatusCodeValue());
+        assertEquals(AppConstant.Message.SUCCESS, Objects.requireNonNull(apiResponse).getMessage());
+        assertEquals(userDto.getUsername(), userDto1.getUsername());
+
+    }
+
+    @Test
+    void updateUserCoverNotFound_Test() throws IOException {
+        UserDao userDao = UserDao.builder()
+                .id(1L)
+                .build();
+
+        when(userRepository.findById(anyLong())).thenReturn(Optional.empty());
+        ResponseEntity<Object> responseEntity = userService.updateUserCover( null, userDao);
+
+        ApiResponse apiResponse = (ApiResponse) responseEntity.getBody();
+
+        assertEquals(HttpStatus.BAD_REQUEST.value(), responseEntity.getStatusCodeValue());
+        assertEquals(AppConstant.Message.NOT_FOUND, Objects.requireNonNull(apiResponse).getMessage());
+
+    }
+
+    @Test
+    void updateUserCoverDenied_Test() throws IOException {
+        UserDao userDao = UserDao.builder()
+                .id(1L)
+                .username("admin")
+                .roles("USER")
+                .build();
+
+        UserDao userDao2 = UserDao.builder()
+                .id(2L)
+                .username("admin")
+                .roles("USER")
+                .build();
+
+        UserDtoResponse userDtoResponse = UserDtoResponse.builder()
+                .id(2L)
+                .username("admin")
+                .build();
+
+        when(userRepository.findById(anyLong())).thenReturn(Optional.of(userDao2));
+
+        ResponseEntity<Object> responseEntity = userService.updateUserCover( null, userDao);
+
+        ApiResponse apiResponse = (ApiResponse) responseEntity.getBody();
+
+        assertEquals(HttpStatus.INTERNAL_SERVER_ERROR.value(), responseEntity.getStatusCodeValue());
+        assertEquals(AppConstant.Message.UNKNOWN_ERROR, Objects.requireNonNull(apiResponse).getMessage());
+
+    }
+
+    @Test
+    void updateUserCoverException_Test() {
+        UserDao userDao = UserDao.builder()
+                .id(1L)
+                .build();
+        when(userRepository.findById(any())).thenThrow(NullPointerException.class);
+        assertThrows(Exception.class, () -> userService.updateUserCover( null, userDao));
+
+    }
+
+    @Test
+    void getUserRankingSuccess_Test() {
+        UserDao userDao = UserDao.builder()
+                .id(1L)
+                .build();
+
+        Page<UserDao> userDaos = new PageImpl<>(List.of(userDao));
+        when(userRepository.findAllUserByRanking(any())).thenReturn(userDaos);
+
+        ResponseEntity<Object> response = userService.getUserRankingByFollowers(any());
+
+        ApiResponse apiResponse = (ApiResponse) response.getBody();
+
+        assertEquals(HttpStatus.OK.value(), response.getStatusCodeValue());
+        assertEquals(AppConstant.Message.SUCCESS, Objects.requireNonNull(apiResponse).getMessage());
+
+    }
+
+    @Test
+    void getUserRankingNotFound_Test() {
+        Page<UserDao> userDaos = new PageImpl<>(List.of());
+        when(userRepository.findAllUserByRanking(any())).thenReturn(userDaos);
+
+        ResponseEntity<Object> response = userService.getUserRankingByFollowers(null);
+
+        ApiResponse apiResponse = (ApiResponse) response.getBody();
+
+        assertEquals(HttpStatus.NOT_FOUND.value(), response.getStatusCodeValue());
+        assertEquals(AppConstant.Message.NOT_FOUND, Objects.requireNonNull(apiResponse).getMessage());
+
+    }
+
+    @Test
+    void getUserRankingException_Test() {
+        when(userRepository.findAllUserByRanking(any())).thenThrow(NullPointerException.class);
+        assertThrows(Exception.class, () -> userService.getUserRankingByFollowers(any()));
+
+    }
+
+    @Test
+    void getUserRankingByTotalThreadAndLikeSuccess_Test() {
+        UserDao userDao = UserDao.builder()
+                .id(1L)
+                .build();
+
+        Page<UserDao> userDaos = new PageImpl<>(List.of(userDao));
+        when(userRepository.findByOrderByTotalThreadsDesc(any())).thenReturn(userDaos);
+
+        ResponseEntity<Object> response = userService.getUserRankingByTotalThreadAndLike(any());
+
+        ApiResponse apiResponse = (ApiResponse) response.getBody();
+
+        assertEquals(HttpStatus.OK.value(), response.getStatusCodeValue());
+        assertEquals(AppConstant.Message.SUCCESS, Objects.requireNonNull(apiResponse).getMessage());
+
+    }
+
+    @Test
+    void getUserRankingByTotalThreadAndLikeNotFound_Test() {
+        Page<UserDao> userDaos = new PageImpl<>(List.of());
+        when(userRepository.findByOrderByTotalThreadsDesc(any())).thenReturn(userDaos);
+
+        ResponseEntity<Object> response = userService.getUserRankingByTotalThreadAndLike(null);
+
+        ApiResponse apiResponse = (ApiResponse) response.getBody();
+
+        assertEquals(HttpStatus.NOT_FOUND.value(), response.getStatusCodeValue());
+        assertEquals(AppConstant.Message.NOT_FOUND, Objects.requireNonNull(apiResponse).getMessage());
+
+    }
+
+    @Test
+    void getUserRankingByTotalThreadAndLikeException_Test() {
+        when(userRepository.findByOrderByTotalThreadsDesc(any())).thenThrow(NullPointerException.class);
+        assertThrows(Exception.class, () -> userService.getUserRankingByTotalThreadAndLike(any()));
+
+    }
 }
