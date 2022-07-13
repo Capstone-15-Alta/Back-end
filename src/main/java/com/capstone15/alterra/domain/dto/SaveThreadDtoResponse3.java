e1aec9d177842afd7f23b4402864411f5d12158a
muhammadhendro <muhammadhendro88@gmail.com>
2022-07-13 15:47:24 +0700

    update response save thread by fe request

---
+++ src/main/java/com/capstone15/alterra/domain/dto/SaveThreadDtoResponse3.java
@@ -0,0 +1,31 @@
+package com.capstone15.alterra.domain.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class SaveThreadDtoResponse3 implements Serializable {
+
+
+    private static final long serialVersionUID = 1541132040032769204L;
+    private Long userId;
+
+    private ThreadDtoResponse thread;
+
+    private Boolean isSave;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss")
+    private LocalDateTime saveTime;
+}
