c3491bb8f8279f61e27a005033b6018865803fb9
muhammadhendro <muhammadhendro88@gmail.com>
2022-07-19 01:02:02 +0700

    fix unit test and update workflow

--- src/test/java/com/capstone15/alterra/service/CommentLikeServiceTest.java
+++ src/test/java/com/capstone15/alterra/service/CommentLikeServiceTest.java
@@ -1,8 +1,11 @@
 package com.capstone15.alterra.service;
 
+import com.capstone15.alterra.constant.AppConstant;
+import com.capstone15.alterra.domain.common.ApiResponse;
 import com.capstone15.alterra.domain.dao.*;
 import com.capstone15.alterra.domain.dto.CategoryDto;
 import com.capstone15.alterra.domain.dto.CommentLikeDto;
+import com.capstone15.alterra.domain.dto.ThreadFollowerDto;
 import com.capstone15.alterra.repository.CommentLikeRepository;
 import com.capstone15.alterra.repository.CommentRepository;
 import com.capstone15.alterra.repository.NotificationRepository;
@@ -18,11 +21,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
-import static org.junit.Assert.*;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(classes = CommentLikeService.class)
@@ -48,174 +52,166 @@ public class CommentLikeServiceTest {
 
     @Test
     void likeComment_Failed_Test() {
-        CommentLikeDao commentLikeDao = CommentLikeDao.builder()
-                .id(1l)
-                .build();
-
         UserDao userDao = UserDao.builder()
-                .id(1l)
+                .id(1L)
                 .build();
-        when(commentLikeRepository.findById(anyLong())).thenReturn(Optional.empty());
 
+        when(commentLikeRepository.findById(anyLong())).thenReturn(Optional.empty());
         ResponseEntity<Object> response = service.likeComment(any(), userDao);
         assertEquals(HttpStatus.BAD_REQUEST.value(), response.getStatusCodeValue());
 
-
     }
 
-
     @Test
-    void likeComment_Sucess_Test() {
+    void likeComment_UpdateIsLikeTrueSuccess_Test() {
         CommentLikeDao commentLikeDao = CommentLikeDao.builder()
-                .id(1l)
+                .id(1L)
+                .isLike(true)
                 .build();
 
         UserDao userDao = UserDao.builder()
-                .id(1l)
+                .id(1L)
                 .build();
+
         CommentDao commentDao = CommentDao.builder()
-                .id(1l)
+                .id(1L)
                 .user(userDao)
                 .build();
+
         NotificationDao notificationDao = NotificationDao.builder()
-                .id(1l)
+                .id(1L)
+                .build();
+
+        when(commentRepository.findById(anyLong())).thenReturn(Optional.of(commentDao));
+        when(commentLikeRepository.findByUserIdAndCommentId(anyLong(), anyLong())).thenReturn(Optional.of(commentLikeDao));
+        when(commentLikeRepository.save(any())).thenReturn(commentLikeDao);
+        when(notificationRepository.findByUserIdAndCommentIdAndInfo(anyLong(), anyLong(), any())).thenReturn(Optional.of(notificationDao));
+        when(notificationRepository.save(any())).thenReturn(notificationDao);
+        when(userRepository.findById(any())).thenReturn(Optional.of(userDao));
+        when(userRepository.save(any())).thenReturn(userDao);
+
+        ResponseEntity<Object> response = service.likeComment(anyLong(), userDao);
+        assertEquals(HttpStatus.OK.value(), response.getStatusCodeValue());
+
+    }
+
+    @Test
+    void likeComment_UpdateIsLikeFalseSuccess_Test() {
+        CommentLikeDao commentLikeDao = CommentLikeDao.builder()
+                .id(1L)
+                .isLike(false)
+                .build();
+
+        UserDao userDao = UserDao.builder()
+                .id(1L)
+                .build();
+
+        CommentDao commentDao = CommentDao.builder()
+                .id(1L)
+                .user(userDao)
+                .build();
+
+        NotificationDao notificationDao = NotificationDao.builder()
+                .id(1L)
+                .build();
+
+        when(commentRepository.findById(anyLong())).thenReturn(Optional.of(commentDao));
+        when(commentLikeRepository.findByUserIdAndCommentId(anyLong(), anyLong())).thenReturn(Optional.of(commentLikeDao));
+        when(commentLikeRepository.save(any())).thenReturn(commentLikeDao);
+        when(notificationRepository.findByUserIdAndCommentIdAndInfo(anyLong(), anyLong(), any())).thenReturn(Optional.of(notificationDao));
+        when(notificationRepository.save(any())).thenReturn(notificationDao);
+        when(userRepository.findById(any())).thenReturn(Optional.of(userDao));
+        when(userRepository.save(any())).thenReturn(userDao);
+
+        ResponseEntity<Object> response = service.likeComment(anyLong(), userDao);
+        assertEquals(HttpStatus.OK.value(), response.getStatusCodeValue());
+
+    }
+
+    @Test
+    void likeComment_NewSuccess_Test() {
+        CommentLikeDao commentLikeDao = CommentLikeDao.builder()
+                .id(1L)
+                .build();
+
+        UserDao userDao = UserDao.builder()
+                .id(1L)
+                .build();
+
+        CommentDao commentDao = CommentDao.builder()
+                .id(1L)
+                .user(UserDao.builder().id(2L).build())
+                .build();
+
+        NotificationDao notificationDao = NotificationDao.builder()
+                .id(1L)
                 .build();
 
         when(commentRepository.findById(anyLong())).thenReturn(Optional.of(commentDao));
         when(commentLikeRepository.findByUserIdAndCommentId(anyLong(), anyLong())).thenReturn(Optional.empty());
         when(commentLikeRepository.save(any())).thenReturn(commentLikeDao);
-        when(commentLikeRepository.countLikes(anyLong())).thenReturn(1);
-        when(commentRepository.save(any())).thenReturn(commentDao);
         when(notificationRepository.save(any())).thenReturn(notificationDao);
-
+        when(userRepository.findById(any())).thenReturn(Optional.of(userDao));
+        when(userRepository.save(any())).thenReturn(userDao);
 
         ResponseEntity<Object> response = service.likeComment(anyLong(), userDao);
         assertEquals(HttpStatus.OK.value(), response.getStatusCodeValue());
 
-
     }
 
     @Test
-    void likeComment_Sucess2_Test() {
-        CommentLikeDao commentLikeDao = CommentLikeDao.builder()
-                .id(1l)
-                .isLike(true)
-                .build();
-
+    void likeComment_Exception_Test() {
         UserDao userDao = UserDao.builder()
-                .id(1l)
-                .build();
-        CommentDao commentDao = CommentDao.builder()
-                .id(1l)
-                .user(userDao)
-                .build();
-        NotificationDao notificationDao = NotificationDao.builder()
-                .id(1l)
-                .build();
-
-        when(commentRepository.findById(anyLong())).thenReturn(Optional.of(commentDao));
-        when(commentLikeRepository.findByUserIdAndCommentId(anyLong(), anyLong())).thenReturn(Optional.of(commentLikeDao));
-        when(commentLikeRepository.save(any())).thenReturn(commentLikeDao);
-        when(commentLikeRepository.countLikes(anyLong())).thenReturn(1);
-        when(commentRepository.save(any())).thenReturn(commentDao);
-        when(notificationRepository.findByUserIdAndCommentIdAndInfo(anyLong(), anyLong(), anyString())).thenReturn(Optional.of(notificationDao));
-        when(notificationRepository.save(any())).thenReturn(notificationDao);
-        ResponseEntity<Object> response = service.likeComment(anyLong(), userDao);
-        assertEquals(HttpStatus.OK.value(), response.getStatusCodeValue());
-
-
-    }
-
-    @Test
-    void likeComment_Sucess3_Test() {
-        CommentLikeDao commentLikeDao = CommentLikeDao.builder()
-                .id(1l)
-                .isLike(false)
-                .build();
-
-        UserDao userDao = UserDao.builder()
-                .id(1l)
-                .build();
-        CommentDao commentDao = CommentDao.builder()
-                .id(1l)
-                .user(userDao)
-                .build();
-        NotificationDao notificationDao = NotificationDao.builder()
-                .id(1l)
-                .build();
-
-        when(commentRepository.findById(anyLong())).thenReturn(Optional.of(commentDao));
-        when(commentLikeRepository.findByUserIdAndCommentId(anyLong(), anyLong())).thenReturn(Optional.of(commentLikeDao));
-        when(commentLikeRepository.save(any())).thenReturn(commentLikeDao);
-        when(commentLikeRepository.countLikes(anyLong())).thenReturn(1);
-        when(commentRepository.save(any())).thenReturn(commentDao);
-        when(notificationRepository.findByUserIdAndCommentIdAndInfo(anyLong(), anyLong(), anyString())).thenReturn(Optional.of(notificationDao));
-        when(notificationRepository.save(any())).thenReturn(notificationDao);
-
-
-        ResponseEntity<Object> response = service.likeComment(anyLong(), userDao);
-        assertEquals(HttpStatus.OK.value(), response.getStatusCodeValue());
-
-
-    }
-
-    @Test
-    void likeComment_Error_Test() {
-
-
-        CommentLikeDao commentLikeDao = CommentLikeDao.builder()
-                .id(1l)
-                .isLike(true)
-                .build();
-
-        UserDao userDao = UserDao.builder()
-                .id(1l)
-                .build();
-        CommentDao commentDao = CommentDao.builder()
-                .id(1l)
-                .user(userDao)
-                .build();
-        NotificationDao notificationDao = NotificationDao.builder()
-                .id(1l)
+                .id(1L)
                 .build();
 
         when(commentRepository.findById(anyLong())).thenThrow(NullPointerException.class);
-        when(commentLikeRepository.findByUserIdAndCommentId(anyLong(), anyLong())).thenReturn(Optional.of(commentLikeDao));
-        when(commentLikeRepository.save(any())).thenReturn(commentLikeDao);
-        when(commentLikeRepository.countLikes(anyLong())).thenReturn(1);
-        when(commentRepository.save(any())).thenReturn(commentDao);
-        when(notificationRepository.save(any())).thenThrow(NullPointerException.class);
         assertThrows(Exception.class, () -> service.likeComment(anyLong(), userDao));
-
     }
 
     @Test
-    void getlikebyidcomment_Failed_Test() {
-        CommentDao commentDao = CommentDao.builder()
-                .id(1l)
-                .build();
-
-        CommentLikeDao commentLikeDao = CommentLikeDao.builder()
-                .id(1l)
-                .build();
-
-
+    void getLikeByIdComment_Failed_Test() {
         when(commentRepository.findById(anyLong())).thenReturn(Optional.empty());
 
         ResponseEntity<Object> response = service.getLikeByIdComment(anyLong());
         assertEquals(HttpStatus.BAD_REQUEST.value(), response.getStatusCodeValue());
     }
 
-
-
     @Test
-    void getlikebyidcomment_Error_Test() {
-
+    void getLikeByIdComment_Exception_Test() {
         when(commentRepository.findById(anyLong())).thenThrow(NullPointerException.class);
         assertThrows(Exception.class, () -> service.getLikeByIdComment(1L));
 
     }
 
+    @Test
+    void getLikeByIdCommentSuccess_Test() {
 
+        CommentLikeDao commentLikeDao = CommentLikeDao.builder()
+                .id(1L)
+                .isLike(true)
+                .build();
+
+        CommentLikeDto commentLikeDto = CommentLikeDto.builder()
+                .isLike(true)
+                .build();
+
+        CommentDao commentDao = CommentDao.builder()
+                .id(1L)
+                .commentLikes(List.of(commentLikeDao))
+                .build();
+
+        when(commentRepository.findById(anyLong())).thenReturn(Optional.of(commentDao));
+        when(mapper.map(any(), eq(CommentLikeDao.class))).thenReturn(commentLikeDao);
+        when(mapper.map(any(), eq(CommentLikeDto.class))).thenReturn(commentLikeDto);
+
+        ResponseEntity<Object> response = service.getLikeByIdComment(anyLong());
+
+        ApiResponse apiResponse = (ApiResponse) response.getBody();
+
+        assertEquals(HttpStatus.OK.value(), response.getStatusCodeValue());
+        assertEquals(AppConstant.Message.SUCCESS, Objects.requireNonNull(apiResponse).getMessage());
+
+    }
 
 }
--- src/test/java/com/capstone15/alterra/service/ThreadLikeServiceTest.java
+++ src/test/java/com/capstone15/alterra/service/ThreadLikeServiceTest.java
@@ -1,13 +1,13 @@
 package com.capstone15.alterra.service;
 
-import com.capstone15.alterra.domain.dao.NotificationDao;
-import com.capstone15.alterra.domain.dao.ThreadDao;
-import com.capstone15.alterra.domain.dao.ThreadLikeDao;
-import com.capstone15.alterra.domain.dao.UserDao;
+import com.capstone15.alterra.constant.AppConstant;
+import com.capstone15.alterra.domain.common.ApiResponse;
+import com.capstone15.alterra.domain.dao.*;
 import com.capstone15.alterra.domain.dto.ThreadDto;
 import com.capstone15.alterra.repository.NotificationRepository;
 import com.capstone15.alterra.repository.ThreadLikeRepository;
 import com.capstone15.alterra.repository.ThreadRepository;
+import com.capstone15.alterra.repository.UserRepository;
 import org.h2.engine.User;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -20,9 +20,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(SpringExtension.class)
@@ -38,133 +40,175 @@ public class ThreadLikeServiceTest {
     private NotificationRepository notificationRepository;
 
     @MockBean
+    private UserRepository userRepository;
+
+    @MockBean
     private ModelMapper mapper;
 
     @Autowired
     private ThreadLikeService service;
 
     @Test
-    void likethread_Failed_Test() {
-
-        ThreadDao threadDao = ThreadDao.builder()
-                .id(1l)
-                .build();
-
+    void likeThread_Failed_Test() {
         UserDao userDao = UserDao.builder()
-                .id(1l)
+                .id(1L)
                 .build();
+
         when(threadRepository.findById(anyLong())).thenReturn(Optional.empty());
         ResponseEntity<Object> response = service.likeThread(anyLong(), userDao);
         assertEquals(HttpStatus.BAD_REQUEST.value(), response.getStatusCodeValue());
     }
 
-
     @Test
-    void likethread_Success_Test() {
-        UserDao userDao = UserDao.builder()
-                .id(1l)
-                .build();
+    void likeThread_UpdateIsLikeFalseSuccess_Test() {
         ThreadDao threadDao = ThreadDao.builder()
-                .id(1l)
-                .user(userDao)
+                .id(1L)
+                .user(UserDao.builder().id(1L).build())
                 .build();
 
-        NotificationDao notificationDao = NotificationDao.builder()
-                .id(1l)
-                .build();
-        when(threadRepository.findById(anyLong())).thenReturn(Optional.of(threadDao));
-        when(notificationRepository.save(any())).thenReturn(notificationDao);
-        ResponseEntity<Object> response = service.likeThread(anyLong(), userDao);
-        assertEquals(HttpStatus.OK.value(), response.getStatusCodeValue());
-    }
-
-    @Test
-    void likethread_Success2_Test() {
         UserDao userDao = UserDao.builder()
-                .id(1l)
+                .id(1L)
                 .build();
-        ThreadDao threadDao = ThreadDao.builder()
-                .id(1l)
-                .user(userDao)
-                .build();
+
         ThreadLikeDao threadLikeDao = ThreadLikeDao.builder()
-                .id(1l)
-                .isLike(true)
-                .build();
-
-        NotificationDao notificationDao = NotificationDao.builder()
-                .id(1l)
-                .build();
-        when(threadRepository.findById(anyLong())).thenReturn(Optional.of(threadDao));
-        when(threadLikeRepository.findByUserIdAndThreadId(anyLong(),anyLong())).thenReturn(Optional.of(threadLikeDao));
-        when(threadLikeRepository.save(any())).thenReturn(threadLikeDao);
-        when(threadLikeRepository.countLikes(anyLong())).thenReturn(12);
-        when(threadRepository.save(any())).thenReturn(threadDao);
-        when(notificationRepository.findByUserIdAndThreadIdAndInfo(anyLong(),anyLong(),anyString())).thenReturn(Optional.of(notificationDao));
-        when(notificationRepository.save(any())).thenReturn(notificationDao);
-        ResponseEntity<Object> response = service.likeThread(anyLong(), userDao);
-        assertEquals(HttpStatus.OK.value(), response.getStatusCodeValue());
-    }
-
-    @Test
-    void likethread_Succes3_Test(){
-        UserDao userDao = UserDao.builder()
-                .id(1l)
-                .build();
-        ThreadDao threadDao = ThreadDao.builder()
-                .id(1l)
-                .user(userDao)
-                .build();
-        ThreadLikeDao threadLikeDao = ThreadLikeDao.builder()
-                .id(1l)
+                .id(1L)
                 .isLike(false)
                 .build();
 
         NotificationDao notificationDao = NotificationDao.builder()
-                .id(1l)
-                .isRead(false)
+                .id(1L)
                 .build();
+
         when(threadRepository.findById(anyLong())).thenReturn(Optional.of(threadDao));
-        when(threadLikeRepository.findByUserIdAndThreadId(anyLong(),anyLong())).thenReturn(Optional.of(threadLikeDao));
+        when(mapper.map(any(), eq(ThreadLikeDao.class))).thenReturn(threadLikeDao);
+        when(threadLikeRepository.findByUserIdAndThreadId(anyLong(), anyLong())).thenReturn(Optional.of(threadLikeDao));
         when(threadLikeRepository.save(any())).thenReturn(threadLikeDao);
-        when(threadLikeRepository.countLikes(anyLong())).thenReturn(12);
-        when(threadRepository.save(any())).thenReturn(threadDao);
-        when(notificationRepository.findByUserIdAndThreadIdAndInfo(anyLong(),anyLong(),anyString())).thenReturn(Optional.of(notificationDao));
+        when(notificationRepository.findByUserIdAndThreadIdAndInfo(anyLong(), anyLong(), any())).thenReturn(Optional.of(notificationDao));
         when(notificationRepository.save(any())).thenReturn(notificationDao);
+        when(userRepository.findById(anyLong())).thenReturn(Optional.of(userDao));
+        when(userRepository.save(any())).thenReturn(userDao);
         ResponseEntity<Object> response = service.likeThread(anyLong(), userDao);
+
+        ApiResponse apiResponse = (ApiResponse) response.getBody();
+
         assertEquals(HttpStatus.OK.value(), response.getStatusCodeValue());
-}
+        assertEquals(AppConstant.Message.SUCCESS, Objects.requireNonNull(apiResponse).getMessage());
+
+    }
+
     @Test
-    void likethread_Exception_Test() {
-        UserDao userDao = UserDao.builder()
-                .id(1l)
-                .build();
+    void likeThread_UpdateIsLikeTrueSuccess_Test() {
         ThreadDao threadDao = ThreadDao.builder()
-                .id(1l)
-                .user(userDao)
+                .id(1L)
+                .user(UserDao.builder().id(1L).build())
                 .build();
+
+        UserDao userDao = UserDao.builder()
+                .id(1L)
+                .build();
+
         ThreadLikeDao threadLikeDao = ThreadLikeDao.builder()
-                .id(1l)
+                .id(1L)
+                .isLike(true)
                 .build();
 
         NotificationDao notificationDao = NotificationDao.builder()
-                .id(1l)
+                .id(1L)
                 .build();
-        when(threadRepository.findById(anyLong())).thenThrow(NullPointerException.class);
-        when(notificationRepository.save(any())).thenReturn(notificationDao);
+
+        when(threadRepository.findById(anyLong())).thenReturn(Optional.of(threadDao));
+        when(mapper.map(any(), eq(ThreadLikeDao.class))).thenReturn(threadLikeDao);
+        when(threadLikeRepository.findByUserIdAndThreadId(anyLong(), anyLong())).thenReturn(Optional.of(threadLikeDao));
         when(threadLikeRepository.save(any())).thenReturn(threadLikeDao);
-        when(threadLikeRepository.countLikes(anyLong())).thenReturn(1);
-        when(threadRepository.save(any())).thenReturn(threadDao);
-        when(notificationRepository.findByUserIdAndThreadIdAndInfo(anyLong(), anyLong(), anyString())).thenThrow(NullPointerException.class);
+        when(notificationRepository.findByUserIdAndThreadIdAndInfo(anyLong(), anyLong(), any())).thenReturn(Optional.of(notificationDao));
+        when(notificationRepository.save(any())).thenReturn(notificationDao);
+        when(userRepository.findById(anyLong())).thenReturn(Optional.of(userDao));
+        when(userRepository.save(any())).thenReturn(userDao);
+        ResponseEntity<Object> response = service.likeThread(anyLong(), userDao);
+
+        ApiResponse apiResponse = (ApiResponse) response.getBody();
+
+        assertEquals(HttpStatus.OK.value(), response.getStatusCodeValue());
+        assertEquals(AppConstant.Message.SUCCESS, Objects.requireNonNull(apiResponse).getMessage());
+
+    }
+
+    @Test
+    void likeThreadNewSuccess_Test() {
+        ThreadDao threadDao = ThreadDao.builder()
+                .id(1L)
+                .user(UserDao.builder().id(1L).build())
+                .build();
+
+        UserDao userDao = UserDao.builder()
+                .id(1L)
+                .build();
+
+        ThreadLikeDao threadLikeDao = ThreadLikeDao.builder()
+                .id(1L)
+                .build();
+
+        when(threadRepository.findById(anyLong())).thenReturn(Optional.of(threadDao));
+        when(threadLikeRepository.findByUserIdAndThreadId(anyLong(), anyLong())).thenReturn(Optional.empty());
+        when(threadLikeRepository.save(any())).thenReturn(threadLikeDao);
+        when(userRepository.findById(anyLong())).thenReturn(Optional.of(userDao));
+        when(userRepository.save(any())).thenReturn(userDao);
+        ResponseEntity<Object> response = service.likeThread(anyLong(), userDao);
+
+        ApiResponse apiResponse = (ApiResponse) response.getBody();
+
+        assertEquals(HttpStatus.OK.value(), response.getStatusCodeValue());
+        assertEquals(AppConstant.Message.SUCCESS, Objects.requireNonNull(apiResponse).getMessage());
+
+    }
+
+    @Test
+    void likeThreadNewNotificationSuccess_Test() {
+        ThreadDao threadDao = ThreadDao.builder()
+                .id(1L)
+                .user(UserDao.builder().id(1L).build())
+                .build();
+
+        UserDao userDao = UserDao.builder()
+                .id(2L)
+                .build();
+
+        ThreadLikeDao threadLikeDao = ThreadLikeDao.builder()
+                .id(1L)
+                .build();
+
+        NotificationDao notificationDao = NotificationDao.builder()
+                .id(1L)
+                .build();
+
+        when(threadRepository.findById(anyLong())).thenReturn(Optional.of(threadDao));
+        when(threadLikeRepository.findByUserIdAndThreadId(anyLong(), anyLong())).thenReturn(Optional.empty());
+        when(threadLikeRepository.save(any())).thenReturn(threadLikeDao);
+        when(notificationRepository.findByUserIdAndThreadIdAndInfo(anyLong(), anyLong(), any())).thenReturn(Optional.of(notificationDao));
+        when(notificationRepository.save(any())).thenReturn(notificationDao);
+        when(userRepository.findById(anyLong())).thenReturn(Optional.of(userDao));
+        when(userRepository.save(any())).thenReturn(userDao);
+        ResponseEntity<Object> response = service.likeThread(anyLong(), userDao);
+
+        ApiResponse apiResponse = (ApiResponse) response.getBody();
+
+        assertEquals(HttpStatus.OK.value(), response.getStatusCodeValue());
+        assertEquals(AppConstant.Message.SUCCESS, Objects.requireNonNull(apiResponse).getMessage());
+
+    }
+
+    @Test
+    void likeThread_Exception_Test() {
+        UserDao userDao = UserDao.builder()
+                .id(1L)
+                .build();
+
+        when(threadRepository.findById(anyLong())).thenThrow(NullPointerException.class);
         assertThrows(Exception.class, () -> service.likeThread(anyLong(), userDao));
     }
 
     @Test
     void getLikeByIdThread_Failed_Test() {
-        ThreadDao threadDao = ThreadDao.builder()
-                .id(1l)
-                .build();
-
         when(threadRepository.findById(anyLong())).thenReturn(Optional.empty());
         ResponseEntity<Object> response = service.getLikeByIdThread(anyLong());
         assertEquals(HttpStatus.BAD_REQUEST.value(), response.getStatusCodeValue());
@@ -174,44 +218,24 @@ public class ThreadLikeServiceTest {
     @Test
     void getLikeByIdThread_Success_Test() {
         ThreadLikeDao threadLikeDao = ThreadLikeDao.builder()
-                .id(1l)
+                .id(1L)
                 .isLike(true)
                 .build();
 
         ThreadDao threadDao = ThreadDao.builder()
-                .id(1l)
+                .id(1L)
                 .threadLikes(List.of(threadLikeDao))
                 .build();
 
-        ThreadDto threadDto = ThreadDto.builder()
-                .id(1l)
-                .build();
         when(threadRepository.findById(anyLong())).thenReturn(Optional.of(threadDao));
-
         ResponseEntity<Object> response = service.getLikeByIdThread(anyLong());
         assertEquals(HttpStatus.OK.value(), response.getStatusCodeValue());
     }
 
     @Test
     void getLikeByIdThread_Exception_Test() {
-        ThreadLikeDao threadLikeDao = ThreadLikeDao.builder()
-                .id(1l)
-                .isLike(true)
-                .build();
-
-        ThreadDao threadDao = ThreadDao.builder()
-                .id(1l)
-                .threadLikes(List.of(threadLikeDao))
-                .build();
-
-        ThreadDto threadDto = ThreadDto.builder()
-                .id(1l)
-                .build();
         when(threadRepository.findById(anyLong())).thenThrow(NullPointerException.class);
-
-
         assertThrows(Exception.class, () -> service.getLikeByIdThread(anyLong()));
     }
-
 
 }
