29adce19eae9b4e131cdce1eb30126114587ff07
muhammadhendro <muhammadhendro88@gmail.com>
2022-07-19 07:16:35 +0700

    update workflow and fix unit test

--- src/test/java/com/capstone15/alterra/service/ThreadReportServiceTest.java
+++ src/test/java/com/capstone15/alterra/service/ThreadReportServiceTest.java
@@ -24,7 +24,8 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(SpringExtension.class)
@@ -50,19 +51,12 @@ public class ThreadReportServiceTest {
 
     @Test
     void addReport_Failed_Test() {
-
         UserDao userDao = UserDao.builder()
-                .id(1l)
+                .id(1L)
                 .build();
 
-        ThreadDao threadDao = ThreadDao.builder()
-                .id(1l)
-                .build();
-        ThreadReportDao threadReportDao = ThreadReportDao.builder()
-                .id(1l)
-                .build();
         ThreadReportDto threadReportDto = ThreadReportDto.builder()
-                .id(1l)
+                .id(1L)
                 .build();
 
         when(threadRepository.findById(anyLong())).thenReturn(Optional.empty());
@@ -73,28 +67,30 @@ public class ThreadReportServiceTest {
 
     @Test
     void addReport_Success_Test() {
-
         UserDao userDao = UserDao.builder()
-                .id(1l)
+                .id(1L)
                 .build();
+
         ThreadReportDao threadReportDao = ThreadReportDao.builder()
-                .id(1l)
+                .id(1L)
                 .user(userDao)
                 .build();
 
         ThreadReportDto threadReportDto = ThreadReportDto.builder()
-                .id(1l)
-                .userId(1l)
+                .id(1L)
+                .userId(1L)
                 .report("unsur kekerasan")
                 .reportTime(LocalDateTime.now())
-                .threadId(1l)
+                .threadId(1L)
                 .build();
+
         ThreadDao threadDao = ThreadDao.builder()
-                .id(1l)
+                .id(1L)
                 .user(userDao)
                 .build();
+
         NotificationDao notificationDao = NotificationDao.builder()
-                .id(1l).
+                .id(1L).
                 build();
 
         when(threadRepository.findById(anyLong())).thenReturn(Optional.of(threadDao));
@@ -104,72 +100,80 @@ public class ThreadReportServiceTest {
         ResponseEntity<Object> response = service.addReport(threadReportDto, userDao);
         assertEquals(HttpStatus.OK.value(), response.getStatusCodeValue());
 
+    }
+
+    @Test
+    void addReportNotificationNotEquals_Success_Test() {
+        UserDao userDao = UserDao.builder()
+                .id(1L)
+                .build();
+
+        ThreadReportDao threadReportDao = ThreadReportDao.builder()
+                .id(1L)
+                .user(userDao)
+                .build();
+
+        ThreadReportDto threadReportDto = ThreadReportDto.builder()
+                .id(1L)
+                .userId(1L)
+                .report("unsur kekerasan")
+                .reportTime(LocalDateTime.now())
+                .threadId(1L)
+                .build();
+
+        ThreadDao threadDao = ThreadDao.builder()
+                .id(1L)
+                .user(UserDao.builder().id(2L).build())
+                .build();
+
+        NotificationDao notificationDao = NotificationDao.builder()
+                .id(1L).
+                build();
+
+        when(threadRepository.findById(anyLong())).thenReturn(Optional.of(threadDao));
+        when(mapper.map(any(),eq(ThreadReportDao.class))).thenReturn(threadReportDao);
+        when(threadReportRepository.save(any())).thenReturn(threadReportDao);
+        when(notificationRepository.save(any())).thenReturn(notificationDao);
+        ResponseEntity<Object> response = service.addReport(threadReportDto, userDao);
+        assertEquals(HttpStatus.OK.value(), response.getStatusCodeValue());
 
     }
 
     @Test
     void addReport_Exception_Test() {
-
         UserDao userDao = UserDao.builder()
-                .id(1l)
-                .build();
-        ThreadReportDao threadReportDao = ThreadReportDao.builder()
-                .id(1l)
-                .user(userDao)
+                .id(1L)
                 .build();
 
         ThreadReportDto threadReportDto = ThreadReportDto.builder()
-                .id(1l)
-                .userId(1l)
+                .id(1L)
+                .userId(1L)
                 .report("unsur kekerasan")
                 .reportTime(LocalDateTime.now())
-                .threadId(1l)
+                .threadId(1L)
                 .build();
-        ThreadDao threadDao = ThreadDao.builder()
-                .id(1l)
-                .user(userDao)
-                .build();
-        NotificationDao notificationDao = NotificationDao.builder()
-                .id(1l).
-                build();
 
         when(threadRepository.findById(anyLong())).thenThrow(NullPointerException.class);
-        when(mapper.map(any(),eq(ThreadReportDao.class))).thenReturn(threadReportDao);
-        when(threadReportRepository.save(any())).thenReturn(threadReportDao);
-        when(notificationRepository.save(any())).thenThrow(NullPointerException.class);
         assertThrows(Exception.class, () -> service.addReport(threadReportDto,userDao));
-
-
     }
+
     @Test
     void getReportByIdThread_Failed_Test(){
-        ThreadDao threadDao = ThreadDao.builder()
-                .id(1l)
-                .build();
-        UserDao userDao = UserDao.builder()
-                .id(1l)
-                .build();
         when(threadRepository.findById(anyLong())).thenReturn(Optional.empty());
         ResponseEntity<Object> response= service.getReportByIdThread(anyLong());
         assertEquals(HttpStatus.BAD_REQUEST.value(),response.getStatusCodeValue());
     }
 
-
     @Test
     void getReportByIdThread_Success_Test(){
-
         ThreadReportDao threadReportDao = ThreadReportDao.builder()
-                .id(1l)
-                .build();
-        UserDao userDao = UserDao.builder()
-                .id(1l)
+                .id(1L)
                 .build();
 
         ThreadDao threadDao = ThreadDao.builder()
-                .id(1l)
+                .id(1L)
                 .threadReports(List.of(threadReportDao))
                 .build();
-
 
         when(threadRepository.findById(anyLong())).thenReturn(Optional.of(threadDao));
         ResponseEntity<Object> response= service.getReportByIdThread(anyLong());
@@ -178,16 +182,6 @@ public class ThreadReportServiceTest {
 
     @Test
     void getReportByIdThread_Exception_Test(){
-        ThreadReportDao threadReportDao = ThreadReportDao.builder()
-                .id(1l)
-                .build();
-        UserDao userDao = UserDao.builder()
-                .id(1l)
-                .build();
-        ThreadDao threadDao = ThreadDao.builder()
-                .id(1l)
-                .build();
-
         when(threadRepository.findById(anyLong())).thenThrow(NullPointerException.class);
         assertThrows(Exception.class,() -> service.getReportByIdThread(anyLong()));
     }
@@ -195,16 +189,11 @@ public class ThreadReportServiceTest {
     @Test
     void updateReport_Failed_Test(){
         UserDao userDao = UserDao.builder()
-                .id(1l)
+                .id(1L)
                 .build();
-        ThreadDao threadDao = ThreadDao.builder()
-                .id(1l)
-                .build();
-        ThreadReportDao threadReportDao = ThreadReportDao.builder()
-                .id(1l)
-                .build();
+
         ThreadReportDto threadReportDto = ThreadReportDto.builder()
-                .id(1l)
+                .id(1L)
                 .build();
 
         when(threadReportRepository.findById(anyLong())).thenReturn(Optional.empty());
@@ -215,122 +204,99 @@ public class ThreadReportServiceTest {
     @Test
     void updateReport_Error_Test(){
         UserDao userDao = UserDao.builder()
-                .id(1l)
+                .id(1L)
                 .roles("MODERATOR")
                 .build();
-        ThreadDao threadDao = ThreadDao.builder()
-                .id(1l)
-                .build();
 
-        UserDao userDao1 = UserDao.builder()
-                .id(2l)
-                .roles("")
-                .build();
         ThreadReportDao threadReportDao = ThreadReportDao.builder()
-                .id(2l)
-                .user(userDao1)
+                .id(2L)
+                .user(UserDao.builder().id(2L).build())
                 .report("")
                 .build();
+
         ThreadReportDto threadReportDto = ThreadReportDto.builder()
-                .id(1l)
-                .userId(2l)
+                .id(1L)
+                .userId(2L)
                 .report("unsur kekerasan")
                 .reportTime(LocalDateTime.now())
-                .threadId(1l)
+                .threadId(1L)
                 .build();
 
         when(threadReportRepository.findById(anyLong())).thenReturn(Optional.of(threadReportDao));
         ResponseEntity<Object> response = service.updateReport(anyLong(),threadReportDto,userDao);
         assertEquals(HttpStatus.INTERNAL_SERVER_ERROR.value(),response.getStatusCodeValue());
     }
+
     @Test
     void updateReport_Success_Test(){
         UserDao userDao = UserDao.builder()
-                .id(1l)
+                .id(1L)
                 .roles("")
                 .build();
+
         ThreadReportDao threadReportDao = ThreadReportDao.builder()
-                .id(1l)
+                .id(1L)
                 .user(userDao)
                 .build();
 
         ThreadReportDto threadReportDto = ThreadReportDto.builder()
-                .id(1l)
-                .userId(1l)
+                .id(1L)
+                .userId(1L)
                 .report("unsur kekerasan")
                 .reportTime(LocalDateTime.now())
-                .threadId(1l)
+                .threadId(1L)
                 .build();
-        ThreadDao threadDao = ThreadDao.builder()
-                .id(1l)
-                .user(userDao)
-                .build();
-        NotificationDao notificationDao = NotificationDao.builder()
-                .id(1l).
-                build();
 
         when(threadReportRepository.findById(anyLong())).thenReturn(Optional.of(threadReportDao));
-        when(threadRepository.save(any())).thenReturn(threadDao);
         ResponseEntity<Object> response = service.updateReport(anyLong(),threadReportDto,userDao);
         assertEquals(HttpStatus.OK.value(),response.getStatusCodeValue());
 
 }
+
     @Test
     void updateReport_Exception_Test(){
         UserDao userDao = UserDao.builder()
-                .id(1l)
+                .id(1L)
                 .roles("")
-                .build();
-        ThreadReportDao threadReportDao = ThreadReportDao.builder()
-                .id(1l)
-                .user(userDao)
                 .build();
 
         ThreadReportDto threadReportDto = ThreadReportDto.builder()
-                .id(1l)
-                .userId(1l)
+                .id(1L)
+                .userId(1L)
                 .report("unsur kekerasan")
                 .reportTime(LocalDateTime.now())
-                .threadId(1l)
+                .threadId(1L)
                 .build();
-        ThreadDao threadDao = ThreadDao.builder()
-                .id(1l)
-                .user(userDao)
-                .build();
-        NotificationDao notificationDao = NotificationDao.builder()
-                .id(1l).
-                build();
 
         when(threadReportRepository.findById(anyLong())).thenThrow(NullPointerException.class);
-        when(threadRepository.save(any())).thenThrow(NullPointerException.class);
         assertThrows(Exception.class, () -> service.updateReport(anyLong(),threadReportDto,userDao));
 
     }
 
     @Test
-    void deletereport_Failed_Test(){
-        ThreadReportDao threadReportDao = ThreadReportDao.builder()
-                .id(1l)
-                .build();
+    void deleteReport_Failed_Test(){
         UserDao userDao = UserDao.builder()
-                .id(1l)
+                .id(1L)
                 .build();
-        when( threadReportRepository.findById(anyLong())).thenReturn(Optional.empty());
+
+        when(threadReportRepository.findById(anyLong())).thenReturn(Optional.empty());
         ResponseEntity<Object> response = service.deleteReport(anyLong(),userDao);
         assertEquals(HttpStatus.BAD_REQUEST.value(), response.getStatusCodeValue());
     }
 
     @Test
-    void deletereport_Error_Test(){
+    void deleteReport_Error_Test(){
         UserDao userDao = UserDao.builder()
-                .id(1l)
+                .id(1L)
                 .roles("MODERATOR")
                 .build();
+
         UserDao userDao1 = UserDao.builder()
-                .id(2l)
+                .id(2L)
                 .build();
+
         ThreadReportDao threadReportDao = ThreadReportDao.builder()
-                .id(1l)
+                .id(1L)
                 .user(userDao1)
                 .build();
 
@@ -338,17 +304,15 @@ public class ThreadReportServiceTest {
         ResponseEntity<Object> response = service.deleteReport(anyLong(),userDao);
         assertEquals(HttpStatus.INTERNAL_SERVER_ERROR.value(), response.getStatusCodeValue());
     }
+
     @Test
-    void deletereport_Success_Test() {
-        UserDao userDao = UserDao.builder()
-                .id(1l)
-                .roles("MODERATOR")
-                .build();
+    void deleteReport_Success_Test() {
         UserDao userDao1 = UserDao.builder()
-                .id(2l)
+                .id(2L)
                 .build();
+
         ThreadReportDao threadReportDao = ThreadReportDao.builder()
-                .id(1l)
+                .id(1L)
                 .user(userDao1)
                 .build();
 
@@ -359,21 +323,13 @@ public class ThreadReportServiceTest {
     }
 
     @Test
-    void deletereport_Exception_Test() {
+    void deleteReport_Exception_Test() {
         UserDao userDao = UserDao.builder()
-                .id(1l)
+                .id(1L)
                 .roles("MODERATOR")
-                .build();
-        UserDao userDao1 = UserDao.builder()
-                .id(2l)
-                .build();
-        ThreadReportDao threadReportDao = ThreadReportDao.builder()
-                .id(1l)
-                .user(userDao1)
                 .build();
 
         when(threadReportRepository.findById(anyLong())).thenThrow(NullPointerException.class);
-        doNothing().when(threadReportRepository).deleteById(anyLong());
         assertThrows(Exception.class, () ->  service.deleteReport(anyLong(),userDao));
     }
 }
